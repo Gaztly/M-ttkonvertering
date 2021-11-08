74e620743268fddc4a2104c292f88c7394496392
BondeJohan <johan.andersson3086@outlook.com>
2021-11-08 11:51:26 +0100

    Add project files.

---
+++ Classes/SwedishMeasurment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Måttkonvertering.Classes
+{
+    class SwedishMeasurment
+    {
+        Dictionary<string, double> Litermått = new Dictionary<string, double>(){
+            { "liter", 1000 },
+            { "deciliter", 100 },
+            { "centiliter", 10},
+            { "matsked", 15 },
+            { "tesked", 5},
+            { "kryddmått", 1},
+
+        };
+
+        Dictionary
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Måttkonvertering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
