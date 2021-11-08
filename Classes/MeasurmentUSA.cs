2a0f5b9d16b59107a7bf3c1b4991f30461677968
Patrick Snäll <patrick.snall@elev.nbi-handelsakademin.se>
2021-11-08 14:33:19 +0100

    NewandImproved

---
+++ Classes/MeasurmentUSA.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Måttkonvertering
+{
+    static class MeasurmentUSA
+    {
+        public static Dictionary<string, double> VolumeInMilliliters { get; } = new Dictionary<string, double>(){
+            { "Gallon", 3800}
+            { "Quart",950}
+            { "Pint", 470}
+            { "Cup",240}
+            { "Fluid Ounce",30}
+        };
+
+        Dictionary<string, double> VolumeInGrams { get; } = new Dictionary<string, double>()
+        {" "}
+         
+    }
+}
